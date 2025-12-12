334b1269f3304b4c30058c27f0bef07bb3b76556
tobby <btae1007@gmail.com>
2025-12-12 17:23:32 +0900

    경로 상 error 컴포넌트 테스트

--- src/app/edu/layout.tsx
+++ src/app/edu/layout.tsx
@@ -3,8 +3,9 @@
 export default function EduLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div className='flex justify-center mb-3'>
+      <div className='flex justify-center mb-3 flex-col items-center gap-10'>
         <p className='font-bold text-2xl'>박태형의 연습장</p>
+        <h2 className='font-bold text-2xl'>EUD LayOut</h2>
       </div>
       {children}
     </div>
--- src/app/edu/test/page.tsx
+++ src/app/edu/test/page.tsx
@@ -1,3 +1,5 @@
-export function TestPage() {
-  return <div>hello</div>;
+export default function TestPage() {
+  throw new Error('일부로 에러를 발생시켜봅니다.');
+
+  return <div>hello Test Page</div>;
 }
