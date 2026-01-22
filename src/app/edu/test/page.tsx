7ddcd755e576e7e280c4432a929a32e30844687a
tobby <btae1007@gmail.com>
2026-01-23 00:08:52 +0900

    서버 코드 제거

--- src/app/edu/test/page.tsx
+++ src/app/edu/test/page.tsx
@@ -2,12 +2,12 @@ import TestUi from "src/features/edu/test";
 
 export const dynamic = "force-static"
 export default async function TestPage() {
-  const resonse = await fetch("http://localhost:8080/edu", {
-    // next: { tags: ["test"], revalidate: 10 },
-    cache :'force-cache'
-  });
-  const data = await resonse.json();
-  console.log(data);
+  // const resonse = await fetch("http://localhost:8080/edu", {
+  //   // next: { tags: ["test"], revalidate: 10 },
+  //   cache :'force-cache'
+  // });
+  // const data = await resonse.json();
+  // console.log(data);
 
   return (
     <div>
