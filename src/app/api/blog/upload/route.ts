c1586a43f61ebd1641eb18f539a56a1d613d32ef
tobby <btae1007@gmail.com>
2026-02-07 23:37:18 +0900

    S3 업로드 추가 및 성공 실패 파일 구분

--- src/app/api/blog/upload/route.ts
+++ src/app/api/blog/upload/route.ts
@@ -15,32 +15,47 @@ export async function POST(req: NextRequest) {
     const formData = await req.formData();
     const files = formData.getAll("files") as File[];
 
-    const fileNames: string[] = [];
+    // 파일 없으면 400 오류
+    if (!files.length) {
+      return NextResponse.json({ message: "업로드할 파일이 없습니다." }, { status: 400 });
+    }
+
     const uploadPromises = files.map(async (file) => {
       const buffer = Buffer.from(await file.arrayBuffer());
       const fileName = file.name; // 클라이언트에서 보낸 'zip이름/파일명'
-      fileNames.push(fileName);
-      console.log(file, "filefile");
       // 1. S3 업로드
-      // await s3.send(
-      //   new PutObjectCommand({
-      //     Bucket: process.env.AWS_S3_BUCKET,
-      //     Key: `uploads/${fileName}`,
-      //     Body: buffer,
-      //     ContentType: file.type,
-      //   }),
-      // );
-
-      // 2. 서버/DB에 특정 값 등록 로직 (예시)
-      // await db.record.create({ data: { name: fileName, url: ... } });
-      console.log(`등록 완료: ${fileName}`);
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: "blog-zip-file",
+          Key: `blog/gits/${fileName}`,
+          Body: buffer,
+          ContentType: file.type,
+        }),
+      );
 
       return fileName;
     });
 
-    await Promise.all(uploadPromises);
+    const results = await Promise.allSettled(uploadPromises);
 
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+    const successFiles: string[] = [];
+    const failedFiles: { fileName: string; reason: string }[] = [];
+
+    results.forEach((result, index) => {
+      const fileName = files[index].name;
+
+      if (result.status === "fulfilled") {
+        successFiles.push(fileName);
+      } else {
+        failedFiles.push({
+          fileName,
+          reason: result.reason instanceof Error ? result.reason.message : String(result.reason),
+        });
+      }
+    });
+
+
+    return NextResponse.json({ message: "Success", successFiles, failedFiles }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
