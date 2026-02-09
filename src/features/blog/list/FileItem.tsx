195db33a1a7dee71ce347d5e345d796519d32cbb
tobby <btae1007@gmail.com>
2026-02-09 23:19:31 +0900

    update 클립보드 복사되도록 기능 추가

--- src/features/blog/list/FileItem.tsx
+++ src/features/blog/list/FileItem.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { getContentInZip } from "src/entities/blog/api";
 import { ZipFileResponse as apiResponse } from "src/entities/blog/types";
 import { ZipFileResponse } from "src/widgets/blog/list/types";
+import toast from "react-hot-toast";
 
 interface Props {
   item: ZipFileResponse;
@@ -19,18 +20,33 @@ export const FileItem = ({ item, isOpen }: Props) => {
   }, []);
 
   return (
-    <div className={'border border-blue-600 p-4'}>
-      <div className={'border border-blue-200 p-4'}>{item.name}</div>
+    <div className={"border border-blue-600 p-4"}>
+      <div className={"border border-blue-200 p-4"}>{item.name}</div>
       {content && (
         <div>
-          <div className={'p-4'}>상세 내용</div>
-          <div className={'p-4'}>
-            <div className={"border p-4 "}>{content.title}</div>
-            <pre className={"border p-4"}>{content.tags.join(',')}</pre>
-            <pre className={"border p-4"}>{content.content}</pre>
+          <div className={"p-4"}>상세 내용</div>
+          <div className={"p-4"}>
+            <button className={"border p-4"} onClick={() => copyText(content.title)}>
+              {content.title}
+            </button>
+            <pre className={"border p-4"} onClick={() => copyText(content.tags.join(","))}>
+              {content.tags.join(",")}
+            </pre>
+            <pre className={"border p-4"} onClick={() => copyText(content.content)}>
+              {content.content}
+            </pre>
           </div>
         </div>
       )}
     </div>
   );
+};
+
+const copyText = async (text: string) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    toast("복사되었습니다.");
+  } catch (e) {
+    toast("복사에 실패했습니다.");
+  }
 };
